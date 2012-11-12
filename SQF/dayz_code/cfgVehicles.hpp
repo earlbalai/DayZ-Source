9148e1f181d89c562d5d118eaf52b72999f266fa
UnclearWall <unclearwall@gmail.com>
2012-11-12 16:41:05 -0600

    Updated Vehicles Lists

--- SQF/dayz_code/cfgVehicles.hpp
+++ SQF/dayz_code/cfgVehicles.hpp
@@ -282,6 +282,44 @@ class CfgVehicles {
 		gunnerCanSee = 2+16+32;
 		driverCanSee = 2+16+32;
 	};
+	//An2_TK_EP1
+	class An2_Base_EP1;
+	class AN2_DZ: An2_Base_EP1
+	{
+		displayName = "AN2_DZ";
+		scope = 2;
+		side = 2;
+		crew = "";
+		typicalCargo[] = {};
+		hiddenSelections[] = {};
+		class TransportMagazines{};
+		class TransportWeapons{};
+		weapons[] = {};
+		magazines[] = {};
+		gunnerHasFlares = false;
+		commanderCanSee = 2+16+32;
+		gunnerCanSee = 2+16+32;
+		driverCanSee = 2+16+32;
+	};
+	//AH6X_EP1
+	class AH6X_EP1{ scope = 0; };
+	class AH6X_DZ: AH6X_EP1
+	{
+		displayName = "AH6X_DZ";
+		scope = 2;
+		side = 2;
+		crew = "";
+		typicalCargo[] = {};
+		hiddenSelections[] = {};
+		class TransportMagazines{};
+		class TransportWeapons{};
+		weapons[] = {};
+		magazines[] = {};
+		gunnerHasFlares = false;
+		commanderCanSee = 2+16+32;
+		gunnerCanSee = 2+16+32;
+		driverCanSee = 2+16+32;
+	};
 	class House {
 		class DestructionEffects;
 	};
