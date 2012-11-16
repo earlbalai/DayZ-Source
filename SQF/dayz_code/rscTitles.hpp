f920cca509f0374e1b49522667ba1bf0afcddfcf
UnclearWall <unclearwall@gmail.com>
2012-11-15 19:27:18 -0600

    Updated Versions

--- SQF/dayz_code/rscTitles.hpp
+++ SQF/dayz_code/rscTitles.hpp
@@ -87,7 +87,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "DayZ 1.7.4-D11";
+			text = "DayZ 1.7.4";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		class CA_TitleMainMenu;
