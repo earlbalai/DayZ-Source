f6371369bfd992d117ad852e1107fa56498d0800
UnclearWall <unclearwall@gmail.com>
2012-12-12 16:22:05 -0600

    updated Change log for todayz build. Updated to todayz build

--- SQF/dayz_code/rscTitles.hpp
+++ SQF/dayz_code/rscTitles.hpp
@@ -87,7 +87,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "DayZ 1.7.5.D1211";
+			text = "DayZ 1.7.5.D1212";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		class CA_TitleMainMenu;
