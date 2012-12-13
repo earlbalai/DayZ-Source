bc6e67f9f519672ff4fb3d8e23d4eb7980157c91
UnclearWall <unclearwall@gmail.com>
2012-12-13 13:43:06 -0600

    updated SQL

--- SQL/Full.sql
+++ SQL/Full.sql
@@ -3,12 +3,64 @@
 -- Server version:               5.5.22 - MySQL Community Server (GPL)
 -- Server OS:                    Win64
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2012-12-13 13:41:32
+-- Date/time:                    2012-12-13 13:38:43
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+
+-- Dumping structure for table default.character_data
+CREATE TABLE IF NOT EXISTS `character_data` (
+  `CharacterID` int(11) NOT NULL AUTO_INCREMENT,
+  `PlayerID` int(11) NOT NULL DEFAULT '0',
+  `PlayerUID` varchar(45) NOT NULL DEFAULT '0',
+  `InstanceID` int(11) NOT NULL DEFAULT '0',
+  `Datestamp` datetime DEFAULT NULL,
+  `LastLogin` datetime NOT NULL,
+  `Inventory` longtext,
+  `Backpack` longtext,
+  `Worldspace` varchar(70) NOT NULL DEFAULT '[]',
+  `Medical` varchar(200) NOT NULL DEFAULT '[]',
+  `Alive` tinyint(4) NOT NULL DEFAULT '1',
+  `Generation` int(11) NOT NULL DEFAULT '1',
+  `LastAte` datetime NOT NULL,
+  `LastDrank` datetime NOT NULL,
+  `KillsZ` int(11) NOT NULL DEFAULT '0',
+  `HeadshotsZ` int(11) NOT NULL DEFAULT '0',
+  `distanceFoot` int(11) NOT NULL DEFAULT '0',
+  `duration` int(11) NOT NULL DEFAULT '0',
+  `currentState` varchar(100) NOT NULL DEFAULT '[]',
+  `KillsH` int(11) NOT NULL DEFAULT '0',
+  `Model` varchar(50) NOT NULL DEFAULT '"Survivor2_DZ"',
+  `KillsB` int(11) NOT NULL DEFAULT '0',
+  `Humanity` int(11) NOT NULL DEFAULT '2500',
+  `last_updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`CharacterID`),
+  KEY `PlayerID` (`PlayerID`),
+  KEY `Alive_PlayerID` (`Alive`,`LastLogin`,`PlayerID`),
+  KEY `PlayerUID` (`PlayerUID`),
+  KEY `Alive_PlayerUID` (`Alive`,`LastLogin`,`PlayerUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table default.character_data: ~0 rows (approximately)
+/*!40000 ALTER TABLE `character_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `character_data` ENABLE KEYS */;
+
+
+-- Dumping structure for table default.logs
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `action` varchar(255) DEFAULT NULL,
+  `user` varchar(255) NOT NULL DEFAULT '',
+  `timestamp` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table default.logs: ~0 rows (approximately)
+/*!40000 ALTER TABLE `logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `logs` ENABLE KEYS */;
+
 
 -- Dumping structure for table default.object_classes
 CREATE TABLE IF NOT EXISTS `object_classes` (
@@ -391,5 +443,295 @@ INSERT INTO `object_spawns_original` (`ObjectUID`, `Classname`, `Worldspace`, `D
 	(312967633988880750, 'Volha_2_TK_CIV_EP1', '[312,[9676.3398,8880.75,0]]', NULL, NULL),
 	(968723604850, 'UH1H_DZ', '[0,[9687.2,3604.85,0]]', NULL, NULL);
 /*!40000 ALTER TABLE `object_spawns_original` ENABLE KEYS */;
+
+
+-- Dumping structure for procedure default.pCleanup
+DELIMITER //
+CREATE DEFINER=`dayz`@`localhost` PROCEDURE `pCleanup`()
+BEGIN
+
+
+#move dead players to character_dead
+#	insert
+#		into character_data 
+#		select * 
+#		from character_data 
+#		WHERE Alive = '0';
+	
+#remove dead players from data table
+	DELETE 
+		FROM character_data 
+		WHERE Alive = '0';	
+	
+#remove vehicles with 100% damage
+	DELETE
+		FROM object_data 
+		WHERE Damage = '1';	
+
+#remove unused vehicles older then 14 days
+	DELETE
+		FROM object_data
+		WHERE DATE(last_updated) < CURDATE() - INTERVAL 14 DAY
+			AND Classname != 'dummy'
+			AND Classname != 'Hedgehog_DZ'
+			AND Classname != 'Wire_cat1'
+			AND Classname != 'Sandbag1_DZ'
+			AND Classname != 'TrapBear';
+
+#remove tents whose owner has been dead for four days
+	DELETE
+		FROM object_data
+		USING object_data, character_data
+		WHERE object_data.Classname = 'TentStorage'
+			AND object_data.CharacterID = character_data.CharacterID
+			AND character_data.Alive = 0
+			AND DATE(character_data.last_updated) < CURDATE() - INTERVAL 4 DAY;
+
+#remove empty tents older than seven days
+	DELETE
+		FROM object_data
+		WHERE Classname = 'TentStorage'
+			AND DATE(last_updated) < CURDATE() - INTERVAL 7 DAY
+			AND Inventory = '[[[],[]],[[],[]],[[],[]]]';
+	
+	DELETE
+		FROM object_data
+		WHERE Classname = 'TentStorage'
+			AND DATE(last_updated) < CURDATE() - INTERVAL 7 DAY
+			AND Inventory = '[]';		
+
+#remove barbed wire older than two days
+	DELETE
+		FROM object_data
+		WHERE Classname = 'Wire_cat1'
+			AND DATE(last_updated) < CURDATE() - INTERVAL 2 DAY;
+			
+#remove Tank Traps older than fifteen days
+	DELETE
+		FROM object_data
+		WHERE Classname = 'Hedgehog_DZ'
+			AND DATE(last_updated) < CURDATE() - INTERVAL 15 DAY;
+
+#remove Sandbags older than twenty days
+	DELETE
+		FROM object_data
+		WHERE Classname = 'Sandbag1_DZ'
+			AND DATE(last_updated) < CURDATE() - INTERVAL 20 DAY;
+
+#remove Bear Traps older than five days
+	DELETE
+		FROM object_data
+		WHERE Classname = 'TrapBear'
+			AND DATE(last_updated) < CURDATE() - INTERVAL 5 DAY;
+
+END//
+DELIMITER ;
+
+
+-- Dumping structure for procedure default.pCleanupOOB
+DELIMITER //
+CREATE DEFINER=`dayz`@`localhost` PROCEDURE `pCleanupOOB`()
+BEGIN
+
+	DECLARE intLineCount	INT DEFAULT 0;
+	DECLARE intDummyCount	INT DEFAULT 0;
+	DECLARE intDoLine			INT DEFAULT 0;
+	DECLARE intWest				INT DEFAULT 0;
+	DECLARE intNorth			INT DEFAULT 0;
+
+	SELECT COUNT(*)
+		INTO intLineCount
+		FROM object_data;
+
+	SELECT COUNT(*)
+		INTO intDummyCount
+		FROM object_data
+		WHERE Classname = 'dummy';
+
+	WHILE (intLineCount > intDummyCount) DO
+	
+		SET intDoLine = intLineCount - 1;
+
+		SELECT ObjectUID, Worldspace
+			INTO @rsObjectUID, @rsWorldspace
+			FROM object_data
+			LIMIT intDoLine, 1;
+
+		SELECT REPLACE(@rsWorldspace, '[', '') INTO @rsWorldspace;
+		SELECT REPLACE(@rsWorldspace, ']', '') INTO @rsWorldspace;
+		SELECT REPLACE(SUBSTRING(SUBSTRING_INDEX(@rsWorldspace, ',', 2), LENGTH(SUBSTRING_INDEX(@rsWorldspace, ',', 2 -1)) + 1), ',', '') INTO @West;
+		SELECT REPLACE(SUBSTRING(SUBSTRING_INDEX(@rsWorldspace, ',', 3), LENGTH(SUBSTRING_INDEX(@rsWorldspace, ',', 3 -1)) + 1), ',', '') INTO @North;
+
+		SELECT INSTR(@West, '-') INTO intWest;
+		SELECT INSTR(@North, '-') INTO intNorth;
+
+		IF (intNorth = 0) THEN
+			SELECT CONVERT(@North, DECIMAL(16,8)) INTO intNorth;
+		END IF;
+
+		IF (intWest > 0 OR intNorth > 15360) THEN
+			DELETE FROM object_data
+				WHERE ObjectUID = @rsObjectUID;
+		END IF;
+			
+		SET intLineCount = intLineCount - 1;
+
+	END WHILE;
+
+END//
+DELIMITER ;
+
+
+-- Dumping structure for table default.player_data
+CREATE TABLE IF NOT EXISTS `player_data` (
+  `playerID` int(11) NOT NULL DEFAULT '0',
+  `playerUID` varchar(45) NOT NULL DEFAULT '0',
+  `playerName` varchar(50) NOT NULL DEFAULT 'Null',
+  `playerMorality` int(11) NOT NULL DEFAULT '0',
+  `playerSex` int(11) NOT NULL DEFAULT '0',
+  KEY `playerID` (`playerID`),
+  KEY `playerUID` (`playerUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table default.player_data: ~0 rows (approximately)
+/*!40000 ALTER TABLE `player_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player_data` ENABLE KEYS */;
+
+
+-- Dumping structure for table default.player_login
+CREATE TABLE IF NOT EXISTS `player_login` (
+  `LoginID` int(11) NOT NULL AUTO_INCREMENT,
+  `PlayerUID` varchar(45) NOT NULL,
+  `CharacterID` int(11) NOT NULL DEFAULT '0',
+  `datestamp` datetime NOT NULL,
+  `Action` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`LoginID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table default.player_login: ~0 rows (approximately)
+/*!40000 ALTER TABLE `player_login` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player_login` ENABLE KEYS */;
+
+
+-- Dumping structure for procedure default.pMain
+DELIMITER //
+CREATE DEFINER=`dayz`@`localhost` PROCEDURE `pMain`()
+    MODIFIES SQL DATA
+BEGIN
+
+# maximum number of INSTANCE id's USED.
+#-----------------------------------------------
+	DECLARE sInstance VARCHAR(8) DEFAULT 391;
+#-----------------------------------------------
+#maximum number of vehicles allowed !!! theoretical max. amount
+#-----------------------------------------------
+	DECLARE iVehSpawnMax INT DEFAULT 100;
+#-----------------------------------------------	
+
+# DECLARE iVehSpawnMin				INT DEFAULT 0;		#ToDo !!!
+	DECLARE iTimeoutMax 				INT DEFAULT 250;	#number of loops before timeout
+	DECLARE iTimeout 						INT DEFAULT 0;		#internal counter for loops done; used to prevent infinite loops - DO NOT CHANGE
+	DECLARE iNumVehExisting 		INT DEFAULT 0;		#internal counter for already existing vehicles - DO NOT CHANGE
+	DECLARE iNumClassExisting 	INT DEFAULT 0;		#internal counter for already existing class types - DO NOT CHANGE
+	DECLARE i INT DEFAULT 1; #internal counter for vehicles spawns - DO NOT CHANGE
+
+#Starts Cleanup
+	CALL pCleanup();
+	
+		SELECT COUNT(*) 				#retrieve the amount of already spawned vehicles...
+			INTO iNumVehExisting
+			FROM object_data 
+			WHERE Instance = sInstance
+			AND Classname != '-'						#exclude dummys
+			AND Classname != 'Hedgehog_DZ'			#exclude hedgehog
+			AND Classname != 'Wire_cat1'				#exclude wirecat
+			AND Classname != 'Sandbag1_DZ'			#exclude Sanbag
+			AND Classname != 'TrapBear'			#exclude trap
+			AND Classname != 'TentStorage';		#exclude TentStorage
+
+		WHILE (iNumVehExisting < iVehSpawnMax) DO		#loop until maximum amount of vehicles is reached
+
+			#select a random vehicle class
+			SELECT Classname, Chance, MaxNum, Damage
+				INTO @rsClassname, @rsChance, @rsMaxNum, @rsDamage
+				FROM object_classes ORDER BY RAND() LIMIT 1;
+
+			#count number of same class already spawned
+			SELECT COUNT(*) 
+				INTO iNumClassExisting 
+				FROM object_data 
+				WHERE Instance = sInstance
+				AND Classname = @rsClassname;
+
+			IF (iNumClassExisting < @rsMaxNum) THEN
+
+				IF (rndspawn(@rschance) = 1) THEN
+				
+					INSERT INTO object_data (ObjectUID, Instance, Classname, Damage, CharacterID, Worldspace, Inventory, Hitpoints, Fuel, Datestamp)
+						SELECT ObjectUID, sInstance, Classname, RAND(@rsDamage), '0', Worldspace, Inventory, Hitpoints, RAND(1), SYSDATE() 
+							FROM object_spawns 
+							WHERE Classname = @rsClassname 
+								AND NOT ObjectUID IN (select objectuid from object_data where instance = sInstance)
+							ORDER BY RAND()
+							LIMIT 0, 1;
+							
+					SELECT COUNT(*) 
+						INTO iNumVehExisting 
+						FROM object_data 
+						WHERE Instance = sInstance
+							AND Classname != '-'						#exclude dummys
+							AND Classname != 'Hedgehog_DZ'			#exclude hedgehog
+							AND Classname != 'Wire_cat1'				#exclude wirecat
+							AND Classname != 'Sandbag1_DZ'			#exclude Sanbag
+							AND Classname != 'TrapBear'			#exclude trap
+							AND Classname != 'TentStorage';		#exclude TentStorage
+		
+					#update number of same class already spawned
+					SELECT COUNT(*) 
+						INTO iNumClassExisting 
+						FROM object_data 
+						WHERE Instance = sInstance
+						AND Classname = @rsClassname;
+				
+				END IF;
+			END IF;	
+			
+			SET iTimeout = iTimeout + 1; #raise timeout counter
+			IF (iTimeout >= iTimeoutMax) THEN
+				SET iNumVehExisting = iVehSpawnMax;
+			END IF;
+		END WHILE;
+	SET i = i + 1;
+END//
+DELIMITER ;
+
+
+-- Dumping structure for function default.rndspawn
+DELIMITER //
+CREATE DEFINER=`dayz`@`localhost` FUNCTION `rndspawn`(`chance` double) RETURNS tinyint(1)
+BEGIN
+
+	DECLARE bspawn tinyint(1) DEFAULT 0;
+
+	IF (RAND() <= chance) THEN
+		SET bspawn = 1;
+	END IF;
+
+	RETURN bspawn;
+
+END//
+DELIMITER ;
+
+
+-- Dumping structure for table default.version
+CREATE TABLE IF NOT EXISTS `version` (
+  `Version` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table default.version: ~1 rows (approximately)
+/*!40000 ALTER TABLE `version` DISABLE KEYS */;
+INSERT INTO `version` (`Version`) VALUES
+	(2);
+/*!40000 ALTER TABLE `version` ENABLE KEYS */;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- SQL/char_tables.sql
+++ SQL/char_tables.sql
@@ -1,55 +1,81 @@
-SET FOREIGN_KEY_CHECKS=0;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.22 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2012-12-13 13:41:54
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for `Player_DATA`
--- ----------------------------
-CREATE TABLE `Player_DATA` (
-  `PlayerUID` varchar(20) NOT NULL DEFAULT '0',
-  `PlayerName` varchar(128) CHARACTER SET utf8 NOT NULL DEFAULT 'Null',
-  `PlayerMorality` int(11) NOT NULL DEFAULT '0',
-  `PlayerSex` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
-  PRIMARY KEY (`PlayerUID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
--- ----------------------------
--- Table structure for `Player_LOGIN`
--- ----------------------------
-CREATE TABLE `Player_LOGIN` (
-  `LoginID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `PlayerUID` varchar(20) NOT NULL,
-  `CharacterID` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `Datestamp` datetime NOT NULL,
-  `Action` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`LoginID`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
-
--- ----------------------------
--- Table structure for `Character_DATA`
--- ----------------------------
-CREATE TABLE `Character_DATA` (
-  `CharacterID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `PlayerUID` varchar(20) NOT NULL DEFAULT '0',
+-- Dumping structure for table default.character_data
+CREATE TABLE IF NOT EXISTS `character_data` (
+  `CharacterID` int(11) NOT NULL AUTO_INCREMENT,
+  `PlayerID` int(11) NOT NULL DEFAULT '0',
+  `PlayerUID` varchar(45) NOT NULL DEFAULT '0',
   `InstanceID` int(11) NOT NULL DEFAULT '0',
   `Datestamp` datetime DEFAULT NULL,
   `LastLogin` datetime NOT NULL,
   `Inventory` longtext,
   `Backpack` longtext,
-  `Worldspace` varchar(128) NOT NULL DEFAULT '[]',
-  `Medical` varchar(300) NOT NULL DEFAULT '[]',
-  `Alive` tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
-  `Generation` int(11) UNSIGNED NOT NULL DEFAULT '1',
+  `Worldspace` varchar(70) NOT NULL DEFAULT '[]',
+  `Medical` varchar(200) NOT NULL DEFAULT '[]',
+  `Alive` tinyint(4) NOT NULL DEFAULT '1',
+  `Generation` int(11) NOT NULL DEFAULT '1',
   `LastAte` datetime NOT NULL,
   `LastDrank` datetime NOT NULL,
-  `KillsZ` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `HeadshotsZ` int(11) UNSIGNED NOT NULL DEFAULT '0',
-  `DistanceFoot` int(11) NOT NULL DEFAULT '0',
-  `Duration` int(11) NOT NULL DEFAULT '0',
-  `CurrentState` varchar(200) NOT NULL DEFAULT '[]',
-  `KillsH` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `KillsZ` int(11) NOT NULL DEFAULT '0',
+  `HeadshotsZ` int(11) NOT NULL DEFAULT '0',
+  `distanceFoot` int(11) NOT NULL DEFAULT '0',
+  `duration` int(11) NOT NULL DEFAULT '0',
+  `currentState` varchar(100) NOT NULL DEFAULT '[]',
+  `KillsH` int(11) NOT NULL DEFAULT '0',
   `Model` varchar(50) NOT NULL DEFAULT '"Survivor2_DZ"',
-  `KillsB` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `KillsB` int(11) NOT NULL DEFAULT '0',
   `Humanity` int(11) NOT NULL DEFAULT '2500',
+  `last_updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`CharacterID`),
-  KEY `PlayerUID` (`PlayerUID`) USING BTREE,
-  KEY `Alive` (`Alive`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+  KEY `PlayerID` (`PlayerID`),
+  KEY `Alive_PlayerID` (`Alive`,`LastLogin`,`PlayerID`),
+  KEY `PlayerUID` (`PlayerUID`),
+  KEY `Alive_PlayerUID` (`Alive`,`LastLogin`,`PlayerUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table default.character_data: ~0 rows (approximately)
+/*!40000 ALTER TABLE `character_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `character_data` ENABLE KEYS */;
+
+
+-- Dumping structure for table default.player_data
+CREATE TABLE IF NOT EXISTS `player_data` (
+  `playerID` int(11) NOT NULL DEFAULT '0',
+  `playerUID` varchar(45) NOT NULL DEFAULT '0',
+  `playerName` varchar(50) NOT NULL DEFAULT 'Null',
+  `playerMorality` int(11) NOT NULL DEFAULT '0',
+  `playerSex` int(11) NOT NULL DEFAULT '0',
+  KEY `playerID` (`playerID`),
+  KEY `playerUID` (`playerUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table default.player_data: ~0 rows (approximately)
+/*!40000 ALTER TABLE `player_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player_data` ENABLE KEYS */;
+
+
+-- Dumping structure for table default.player_login
+CREATE TABLE IF NOT EXISTS `player_login` (
+  `LoginID` int(11) NOT NULL AUTO_INCREMENT,
+  `PlayerUID` varchar(45) NOT NULL,
+  `CharacterID` int(11) NOT NULL DEFAULT '0',
+  `datestamp` datetime NOT NULL,
+  `Action` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`LoginID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table default.player_login: ~0 rows (approximately)
+/*!40000 ALTER TABLE `player_login` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player_login` ENABLE KEYS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
