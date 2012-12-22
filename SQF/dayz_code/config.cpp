3efbfd5cca4a4feec8c1bec2b0c262849e45433c
UnclearWall <unclearwall@gmail.com>
2012-12-22 13:48:45 -0600

    Updated Versions

--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -34,7 +34,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "1.7.5.D1216";
+		version = "1.7.5.D1222";
 		hiveVersion = 0.96; //0.93
 	};
 };
