f6a3422b295c241ac1c13abe44a321bafebb5332
UnclearWall <unclearwall@gmail.com>
2013-01-11 01:38:52 -0600

    Updated Versions to M1D11

--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -34,7 +34,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "1.7.5.M1D10";
+		version = "1.7.5.M1D11";
 		hiveVersion = 0.96; //0.93
 	};
 };
