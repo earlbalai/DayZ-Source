7030c9d7f282a2f209ffd883ae119a6e01bc799a
UnclearWall <unclearwall@gmail.com>
2013-01-25 13:21:56 -0600

    Updated Versions

--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -34,7 +34,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "1.7.5.M1D21";
+		version = "1.7.5.ReleaseData-1st-M1D25";
 		hiveVersion = 0.96; //0.93
 	};
 };
