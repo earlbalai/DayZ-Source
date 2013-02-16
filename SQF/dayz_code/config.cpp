9e06ab4ba1e04fb2facbf4792b6f6530a5093d6b
UnclearWall <unclearwall@gmail.com>
2013-02-16 09:44:52 -0600

    Updated Version info

--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -41,7 +41,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "1.7.5.2-TestVersion5";
+		version = "1.7.6-PreRelease2";
 		hiveVersion = 0.96; //0.93
 	};
 	class DZ_InitWorld
