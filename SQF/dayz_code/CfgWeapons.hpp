845bf84d1ff517cf626b5ae59eb9150a380b28eb
ziellos2k <marceldevries@my89.nl>
2013-02-18 12:43:05 +0100

    Disable the need to reload the hatchet, which is pointless.

--- SQF/dayz_code/CfgWeapons.hpp
+++ SQF/dayz_code/CfgWeapons.hpp
@@ -18,4 +18,90 @@ class CfgWeapons {
 			"WoodenArrow"
 		};
 	};
+
+	class MeleeWeapon;
+	class MeleeHatchet: MeleeWeapon
+	{
+		scope=2;
+		autoreload=1;
+		magazineReloadTime=0;
+		model="\dayz_weapons\models\Hatchet_weaponized";
+		picture="\dayz_equip\textures\equip_hatchet_CA.paa";
+		displayName="$STR_EQUIP_NAME_41";
+		magazines[]=
+		{
+			"Hatchet_Swing"
+		};
+		handAnim[]=
+		{
+			"OFP2_ManSkeleton",
+			"\dayz_weapons\anim\melee_hatchet_holding.rtm"
+		};
+		class ItemActions
+		{
+			class Use
+			{
+				text="$STR_ACTIONS_CHOPWOOD";
+				script="spawn player_chopWood;";
+			};
+			class Toolbelt
+			{
+				text="Add to Toolbelt";
+				script="spawn player_addToolbelt;";
+				use[]=
+				{
+					"MeleeHatchet"
+				};
+				output[]=
+				{
+					"ItemHatchet"
+				};
+			};
+			class Drop
+			{
+				text="Drop Hatchet";
+				script="spawn player_dropWeapon;";
+				use[]=
+				{
+					"Hatchet_Swing"
+				};
+			};
+		};
+		class Library
+		{
+			libTextDesc="$STR_EQUIP_DESC_41";
+		};
+		descriptionShort="$STR_EQUIP_DESC_41";
+	};
+	
+	class MeleeCrowbar: MeleeHatchet
+	{
+		scope=2;
+		autoreload=1;
+		magazineReloadTime=0;
+		model="\dayz_weapons\models\crowbar_weaponized";
+		picture="\dayz_weapons\textures\equip_crowbar_CA.paa";
+		displayName="Crowbar";
+		magazines[]=
+		{
+			"Crowbar_Swing"
+		};
+		class ItemActions
+		{
+			class Drop
+			{
+				text="Drop Crowbar";
+				script="spawn player_dropWeapon;";
+				use[]=
+				{
+					"Crowbar_Swing"
+				};
+			};
+		};
+		class Library
+		{
+			libTextDesc="A tool consisting of a metal bar with a single curved end and flattened points, often with a small fissure on one or both ends for removing nails.";
+		};
+		descriptionShort="A tool consisting of a metal bar with a single curved end and flattened points, often with a small fissure on one or both ends for removing nails.";
+	};
 };
