24a8cc6550f170ae976468afbb6ce9bec4a74a1b
Elvaron <jonas.janus@googlemail.com>
2013-02-23 17:24:32 +0100

    Razor blade and toilet paper added

--- SQF/dayz_code/CfgMagazines.hpp
+++ SQF/dayz_code/CfgMagazines.hpp
@@ -418,4 +418,23 @@ class CfgMagazines {
 		shortNameMagazine = "Machete";
 		ammo = "Machete_Swing_Ammo";
 	};
+	
+	/*class ItemTrashToiletpaper : CA_Magazine {
+		scope = public;
+		count = 1;
+		displayName = $STR_JUNK_NAME_TOILETPAPER;
+		descriptionShort = $STR_JUNK_DESC_TOILETPAPER;
+		model = "z\addons\dayz_communityassets\models\toiletpaper.p3d";
+		picture = "\z\addons\dayz_communityassets\pictures\equip_toiletpaper_CA.paa";
+		type = 256;
+	};*/
+	/*class ItemTrashRazor : CA_Magazine {
+		scope = public;
+		count = 1;
+		displayName = $STR_JUNK_NAME_RAZOR;
+		descriptionShort = $STR_JUNK_DESC_RAZOR;
+		model = "z\addons\dayz_communityassets\models\razor.p3d";
+		picture = "\z\addons\dayz_communityassets\pictures\equip_razor_CA.paa";
+		type = 256;
+	};*/	
 };
