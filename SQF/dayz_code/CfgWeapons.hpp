da7b45a0f2c69f400a675c32ec9a1f2820b60c4d
AlexAFlorov <alexander.florov@gmail.com>
2013-03-08 15:19:20 +0200

    nicer crowbar toolbelt icon

--- SQF/dayz_code/CfgWeapons.hpp
+++ SQF/dayz_code/CfgWeapons.hpp
@@ -139,7 +139,7 @@ class CfgWeapons {
 		scope=2;
 		displayName=$STR_EQUIP_NAME_CROWBAR;
 		model="\dayz_equip\models\crowbar.p3d";
-		picture="\dayz_weapons\textures\equip_crowbar_CA.paa";
+		picture="\z\addons\dayz_communityassets\pictures\equip_crowbar_CA.paa";
 		descriptionShort=$STR_EQUIP_DESC_CROWBAR;
 		class ItemActions
 		{
