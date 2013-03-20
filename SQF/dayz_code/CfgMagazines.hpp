c09acf3631e3ff387117852deca0c92310fc7570
R4Z0R49 <r4z0r49@gmail.com>
2013-03-20 13:08:59 -0500

    New fuel can updates

--- SQF/dayz_code/CfgMagazines.hpp
+++ SQF/dayz_code/CfgMagazines.hpp
@@ -929,20 +929,24 @@ class CfgMagazines {
 	class ItemFuelcan : CA_Magazine {
 		scope = public;
 		count = 1;
+		fuelQuantity = 5;
+		emptycan = "ItemFuelcanEmpty";
+		type = (256 * 3);
 		displayName = $STR_ITEM_NAME_FUELCAN;
 		descriptionShort = $STR_ITEM_DESC_FUELCAN;
 		model = "z\addons\dayz_communityassets\models\fuelcan.p3d";
 		picture = "\z\addons\dayz_communityassets\pictures\equip_fuelcan_CA.paa";
-		type = 256;
 	};
-	class ItemFuelcanEmpty : CA_Magazine {
+	class ItemFuelcanEmpty :  ItemFuelcan {
 		scope = public;
 		count = 1;
+		fuelQuantity = 0;
+		fullcan = "ItemFuelcan";
+		type = (256 * 3);
 		displayName = $STR_ITEM_NAME_FUELCAN_EMPTY;
 		descriptionShort = $STR_ITEM_DESC_FUELCAN_EMPTY;
 		model = "z\addons\dayz_communityassets\models\fuelcan.p3d";
 		picture = "\z\addons\dayz_communityassets\pictures\equip_fuelcan_empty_CA.paa";
-		type = 256;
 	};
 	class 8Rnd_B_Beneli_Pellets: CA_Magazine {
 		class ItemActions {
--- SQF/dayz_code/actions/jerry_fill.sqf
+++ SQF/dayz_code/actions/jerry_fill.sqf
@@ -3,14 +3,28 @@ private["_qty","_dis","_sfx"];
 player removeAction s_player_fillfuel;
 s_player_fillfuel = -1;
 
-_qty = {_x == "ItemJerrycanEmpty"} count magazines player;
+_fuelcans = ["ItemFuelcanEmpty","ItemJerrycanEmpty"];
 
-if ("ItemJerrycanEmpty" in magazines player) then {
-	for "_x" from 1 to _qty do {
+_qty = 0;
+_qty = {_x in _fuelcans} count magazines player;
+
+_qty20 = {_x == "ItemJerrycanEmpty"} count magazines player;
+_qty5 = {_x == "ItemFuelcanEmpty"} count magazines player;
+
+if (("ItemJerrycanEmpty" in magazines player) or ("ItemFuelcanEmpty" in magazines player)) then {
+	for "_x" from 1 to _qty20 do {
 		player removeMagazine "ItemJerrycanEmpty";
 		player addMagazine "ItemJerrycan";
 	};
+	for "_x" from 1 to _qty5 do {
+		player removeMagazine "ItemFuelcanEmpty";
+		player addMagazine "ItemFuelcan";
+	};
+	
 	player playActionNow "Medic";
+	
+	sleep 6;
+	
 	
 	_dis=10;
 	_sfx = "refuel";
