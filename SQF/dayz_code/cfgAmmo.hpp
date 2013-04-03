a6a36fa1cc31fe9bad1594763e67a12b4af5a64b
R4Z0R49 <r4z0r49@gmail.com>
2013-04-03 14:34:09 -0500

    Melee weapons update.

--- SQF/dayz_code/cfgAmmo.hpp
+++ SQF/dayz_code/cfgAmmo.hpp
@@ -7,7 +7,7 @@ class CfgAmmo {
 	};
 	class Melee;
 	class Machete_Swing_Ammo : Melee {
-		hit = 9;
+		hit = 15;
 		simulation = "shotBullet";
 		minRange = 1;
 		minRangeProbab = 0.8;
@@ -18,6 +18,36 @@ class CfgAmmo {
 		explosive = "true";
 		simulationStep = 0.001;
 		timeToLive = 0.04;
+		soundHit[] = {"", db-90, 1};
+		soundEngine[] = {"", db-80, 4};
+	};
+	class Hatchet_Swing_Ammo : Melee {
+		hit = 12;
+		simulation = "shotBullet";
+		minRange = 1;
+		minRangeProbab = 0.9;
+		midRange = 2;
+		midRangeProbab = 1.9;
+		maxRange = 3;
+		maxRangeProbab = 2.9;
+		explosive = "true";
+		simulationStep = 0.001;
+		timeToLive = 0.05;
+		soundHit[] = {"", db-90, 1};
+		soundEngine[] = {"", db-80, 4};
+	};
+	class Crowbar_Swing_Ammo : Melee {
+		hit = 9;
+		simulation = "shotBullet";
+		minRange = 1;
+		minRangeProbab = 0.8;
+		midRange = 2;
+		midRangeProbab = 1.5;
+		maxRange = 3;
+		maxRangeProbab = 2.5;
+		explosive = "true";
+		simulationStep = 0.001;
+		timeToLive = 0.03;
 		soundHit[] = {"", db-90, 1};
 		soundEngine[] = {"", db-80, 4};
 	};
