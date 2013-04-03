6cbafeb4c468c4e102af92b8f873198dc4dcd1ce
R4Z0R49 <r4z0r49@gmail.com>
2013-04-03 12:32:06 -0500

    Updated Versions

--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -47,7 +47,7 @@ class CfgMods
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "1.7.7";
+		version = "1.7.8.Dev";
 		hiveVersion = 0.96; //0.93
 	};
 	class DZ_InitWorld
