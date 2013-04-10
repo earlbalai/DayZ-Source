f1f3751bfc1a02e1f05cf2a91fa7a21b96e9070d
R4Z0R49 <r4z0r49@gmail.com>
2013-04-10 06:12:37 -0500

    Updated CfgMagazines.hpp to be a little more readable. Updated player_drink to remove instring checks added new config sfxdis to set distance of sound effects

---
+++ SQF/dayz_code/CfgMagazines/MeleeSwing.hpp
@@ -0,0 +1,18 @@
+class Machete_Swing : Melee_Swing {
+	displayName = Machete;
+	displayNameMagazine = Machete;
+	shortNameMagazine = Machete;
+	ammo = Machete_Swing_Ammo;
+};
+class crowbar_swing : Melee_Swing {
+	displayName = Crowbar;
+	displayNameMagazine = Crowbar;
+	shortNameMagazine = Crowbar;
+	ammo = Crowbar_Swing_Ammo;
+};
+class Hatchet_Swing : Melee_Swing {
+	displayName = Hatchet;
+	displayNameMagazine = Hatchet;
+	shortNameMagazine = Hatchet;
+	ammo = Hatchet_Swing_Ammo;
+};
