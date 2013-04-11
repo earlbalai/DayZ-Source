ce11913ae76d54b6255c399b4c6ca881cbd76453
Joe Horan <joeyslucky22@gmail.com>
2013-04-11 00:19:43 -0500

    Added Box of Nails
    Description = A box of 4 inch nails

--- SQF/dayz_code/CfgMagazines/CfgMagazines.hpp
+++ SQF/dayz_code/CfgMagazines/CfgMagazines.hpp
@@ -247,6 +247,15 @@ class CfgMagazines {
 		picture = "\z\addons\dayz_communityassets\pictures\equip_bible_CA.paa";
 		type = 256;
 	};
+	class ItemNails : CA_Magazine {
+		scope = public;
+		count = 1;
+		displayName = $STR_ITEM_NAME_NAILS;
+		descriptionShort = $STR_ITEM_DESC_NAILS;
+		model = "z\addons\dayz_communityassets\models\nails.p3d";
+		picture = "\z\addons\dayz_communityassets\pictures\equip_nails_ca.paa";
+		type = 256;
+	};
 	
 	class ItemJerrycan : CA_Magazine {
 		emptycan = "ItemJerrycanEmpty";
