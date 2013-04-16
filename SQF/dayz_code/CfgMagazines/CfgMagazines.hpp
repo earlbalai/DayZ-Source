9737e7dc490fcd441fd4b64dca952abaa1db22ed
R4Z0R49 <r4z0r49@gmail.com>
2013-04-16 18:05:44 -0500

    Updated melee systems

--- SQF/dayz_code/CfgMagazines/CfgMagazines.hpp
+++ SQF/dayz_code/CfgMagazines/CfgMagazines.hpp
@@ -6,9 +6,9 @@ class CfgMagazines {
 	class TrashTinCan; //External class reference
 	
 	class Melee_Swing : CA_Magazine {
-		scopeWeapon = protected;
-		scopeMagazine = protected;
-		magazineType = WeaponNoSlot;
+	//	scopeWeapon = protected;
+	//	scopeMagazine = protected;
+	//	magazineType = WeaponNoSlot;
 		type = VSoft;
 		flash = "";
 		flashSize = 0;
