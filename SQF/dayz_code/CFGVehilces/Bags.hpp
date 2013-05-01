920ed0d9b4e3bea2a05e7070a19bdbaddfba9817
R4Z0R49 <r4z0r49@gmail.com>
2013-05-01 10:32:58 -0500

    Updated vehicles to be in separate and changed max speeds of a few vehicles

---
+++ SQF/dayz_code/CFGVehilces/Bags.hpp
@@ -0,0 +1,95 @@
+class DZ_Patrol_Pack_EP1: Bag_Base_EP1
+{
+	scope = 2;
+	displayName = "Patrol Pack (coyote)";
+	picture = "\ca\weapons_e\data\icons\backpack_US_ASSAULT_COYOTE_CA.paa";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	mapsize = 2;
+	model = "\ca\weapons_e\AmmoBoxes\backpack_us_assault_Coyote.p3d";
+	transportMaxWeapons = 1;
+	transportMaxMagazines = 8;
+};
+
+class DZ_Assault_Pack_EP1: Bag_Base_EP1
+{
+	scope = 2;
+	displayName = "Assault Pack (ACU)";
+	picture = "\ca\weapons_e\data\icons\backpack_US_ASSAULT_CA.paa";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	mapSize = 2;
+	model = "\ca\weapons_e\AmmoBoxes\backpack_us_assault.p3d";
+	transportMaxWeapons = 1;
+	transportMaxMagazines = 12;
+};
+
+class DZ_Czech_Vest_Puch: Bag_Base_EP1
+{
+	displayname = "Czech Vest Pouch";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	mapsize = 2;
+	model = "\ca\weapons_e\AmmoBoxes\backpack_acr_small.p3d";
+	picture = "\ca\weapons_e\data\icons\backpack_ACR_small_CA.paa";
+	scope = 2;
+	transportmaxmagazines = 12;
+	transportmaxweapons = 0;
+};
+
+class DZ_ALICE_Pack_EP1: Bag_Base_EP1
+{
+	scope = 2;
+	displayName = "ALICE Pack";
+	picture = "\ca\weapons_e\data\icons\backpack_TK_ALICE_CA.paa";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	mapsize = 2;
+	model = "\ca\weapons_e\AmmoBoxes\backpack_tk_alice.p3d";
+	transportMaxWeapons = 2;
+	transportMaxMagazines = 16;
+};
+
+class DZ_TK_Assault_Pack_EP1 : Bag_Base_EP1
+{
+	scope = 2;
+	displayName = "Survival ACU";
+	mapSize = 2;
+	picture = "\ca\weapons_e\data\icons\backpack_CIVIL_ASSAULT_CA.paa";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	model = "\ca\weapons_e\AmmoBoxes\backpack_civil_assault.p3d";
+	transportMaxWeapons = 2;
+	transportMaxMagazines = 16;
+};
+
+class DZ_British_ACU : Bag_Base_EP1
+{
+	scope = 2;
+	displayName = "British Assault Pack";
+	mapSize = 2;
+	model = "\ca\weapons_baf\Backpack_Small_BAF";
+	picture = "\ca\weapons_baf\data\UI\backpack_BAF_CA.paa";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	transportMaxWeapons = 3;
+	transportMaxMagazines = 18;
+};
+
+class DZ_CivilBackpack_EP1: Bag_Base_EP1    
+{
+	scope = 2;
+	displayName = "Czech Backpack";
+	picture = "\ca\weapons_e\data\icons\backpack_ACR_CA.paa";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	mapsize = 2;
+	model = "\ca\weapons_e\AmmoBoxes\backpack_acr.p3d";
+	transportMaxWeapons = 4;
+	transportMaxMagazines = 24;
+};
+
+class DZ_Backpack_EP1: Bag_Base_EP1
+{
+	scope = 2;
+	displayName = "Backpack (coyote)";
+	picture = "\ca\weapons_e\data\icons\backpack_US_CA.paa";
+	icon = "\ca\weapons_e\data\icons\mapIcon_backpack_CA.paa";
+	mapsize = 2;
+	model = "\ca\weapons_e\AmmoBoxes\backpack_us.p3d";
+	transportMaxWeapons = 6;
+	transportMaxMagazines = 24;
+};
---
+++ SQF/dayz_code/CFGVehilces/Skins.hpp
@@ -0,0 +1,63 @@
+class GER_Soldier_EP1;
+class Bandit1_DZ : GER_Soldier_EP1 {
+	displayName = "$STR_CHAR_2";
+	side = 1;
+	weapons[] = {"Throw","Put"};
+	//model = "\dayz\characters\man_bandit";
+	//portrait = "\Ca\characters_E\data\portraits\ger_soldier_CA";
+	magazines[] = {};
+	backpack = "";
+	respawnWeapons[] = {"Throw","Put"};
+	respawnMagazines[] = {};
+	weaponSlots = "1	 + 	4	 + 12*		256	 + 2*	4096	 + 	2	 + 8*	16  + 12*131072";
+	canHideBodies = 1;
+};
+
+class BAF_Soldier_Officer_W;
+class Rocket_DZ: BAF_Soldier_Officer_W {
+	displayName = "Rocket";
+	side = 1;
+	weapons[] = {"Throw","Put"};
+	backpack = "";
+	magazines[] = {};
+	respawnWeapons[] = {"Throw","Put"};
+	respawnMagazines[] = {};
+	weaponSlots = "1	 + 	4	 + 12*		256	 + 2*	4096	 + 	2	 + 8*	16  + 12*131072";
+	canHideBodies = 1;
+};
+class BAF_Soldier_W;
+class Soldier1_DZ: BAF_Soldier_W {
+	displayName = "Soldier";
+	side = 1;
+	weapons[] = {"Throw","Put"};
+	backpack = "";
+	magazines[] = {};
+	respawnWeapons[] = {"Throw","Put"};
+	respawnMagazines[] = {};
+	weaponSlots = "1	 + 	4	 + 12*		256	 + 2*	4096	 + 	2	 + 8*	16  + 12*131072";
+	canHideBodies = 1;
+};
+class BAF_Soldier_SniperH_W;
+class Sniper1_DZ: BAF_Soldier_SniperH_W {
+	displayName = "Sniper";
+	side = 1;
+	weapons[] = {"Throw","Put"};
+	backpack = "";
+	magazines[] = {};
+	respawnWeapons[] = {"Throw","Put"};
+	respawnMagazines[] = {};
+	weaponSlots = "1	 + 	4	 + 12*		256	 + 2*	4096	 + 	2	 + 8*	16  + 12*131072";
+	canHideBodies = 1;
+};
+class BAF_Soldier_L_W;
+class Camo1_DZ: BAF_Soldier_L_W {
+	displayName = "Survivor";
+	side = 1;
+	weapons[] = {"Throw","Put"};
+	backpack = "";
+	magazines[] = {};
+	respawnWeapons[] = {"Throw","Put"};
+	respawnMagazines[] = {};
+	weaponSlots = "1	 + 	4	 + 12*		256	 + 2*	4096	 + 	2	 + 8*	16  + 12*131072";
+	canHideBodies = 1;
+};	
