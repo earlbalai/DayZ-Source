6054d750fa78438e35c0ff4795858f7bada15e1c
Skaronator <DayZ@Skaronator.com>
2013-05-04 17:47:33 +0200

    More CfgVehicles

---
+++ SQF/dayz_code/Configs/CfgVehicles/Helicopter/MI8Wreck.hpp
@@ -0,0 +1,10 @@
+class Mi8Wreck_DZ: SpawnableWreck
+	{
+		model = "\Ca\air\MI8Wreck.p3d";
+		icon = "ca\Misc_E\data\Icons\Icon_uh60_wreck_CA";
+		mapSize = 15;
+		displayName = "Mi-8 Wreck";
+		vehicleClass = "Wrecks";
+		heightAdjustment = 1;
+		class AnimationSources {};
+	};
