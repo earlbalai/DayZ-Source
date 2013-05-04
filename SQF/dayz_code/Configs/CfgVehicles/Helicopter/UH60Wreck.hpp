89bcc5d735ac8526f62a8b657d00e5a9a35cf171
Skaronator <DayZ@Skaronator.com>
2013-05-04 17:47:33 +0200

    More CfgVehicles

---
+++ SQF/dayz_code/Configs/CfgVehicles/Helicopter/UH60Wreck.hpp
@@ -0,0 +1,9 @@
+class UH60Wreck_DZ: SpawnableWreck
+	{
+		model = "\Ca\Misc_E\Wreck_UH60_EP1.p3d";
+		icon = "ca\Misc_E\data\Icons\Icon_uh60_wreck_CA";
+		mapSize = 15;
+		displayName = "UH-60 Wreck";
+		vehicleClass = "Wrecks";
+		class AnimationSources {};
+	};
