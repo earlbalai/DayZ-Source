dc9160d71e02b04e416a9e7c75feea4af59c3b25
Skaronator <DayZ@Skaronator.com>
2013-05-05 11:11:24 +0200

    CfgVehicles ~Done

---
+++ SQF/dayz_code/Configs/CfgVehicles/Dog.hpp
@@ -0,0 +1,9 @@
+class DZ_Fin : Fin {
+		scope = 2;
+		model = "\ca\animals2\Dogs\Fin\Fin";
+		displayName = "Fin";
+		moves = "CfgMovesDogDZ";
+		gestures = "CfgGesturesDogDZ";
+		fsmDanger = "";
+		fsmFormation = "";
+	};
