fbbb6b5b2405cf5685b551e3ffadec364090c36d
Skaronator <DayZ@Skaronator.com>
2013-05-05 11:18:20 +0200

    CfgMagazines ~Done

---
+++ SQF/dayz_code/Configs/CfgMagazines/Jerrycan.hpp
@@ -0,0 +1,11 @@
+class ItemJerrycan : CA_Magazine {
+	emptycan = "ItemJerrycanEmpty";
+	displayName = $STR_ITEM_NAME_JERRYCAN;
+	descriptionShort = $STR_ITEM_DESC_JERRYCAN;
+}; 
+
+class ItemJerrycanEmpty : ItemJerrycan {
+	fullcan = "ItemJerrycan";
+	displayName = $STR_ITEM_NAME_JERRYCAN_EMPTY;
+	descriptionShort = $STR_ITEM_DESC_JERRYCAN_EMPTY;
+};
