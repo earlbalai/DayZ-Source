c68e9e8e69548c459b31391c293e5922a550b235
R4Z0R49 <r4z0r49@gmail.com>
2013-06-05 03:58:23 -0500

    Updated base builds

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -182,7 +182,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "1.7.7.Pre-Release.01062013";
+			text = "1.7.7.Pre-Release.05062013";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		delete CA_TitleMainMenu;
