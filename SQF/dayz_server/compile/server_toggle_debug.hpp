ea27d7ce3ce977d65a9b7a78d06e337c6b5bf72d
dayz10k <dayz10000@gmail.com>
2013-06-14 12:10:31 -0300

    Create server_toggle_debug.hpp

---
+++ SQF/dayz_server/compile/server_toggle_debug.hpp
@@ -0,0 +1,15 @@
+/*
+Created exclusively for ArmA2:OA - DayZMod.
+Please request permission to use/alter/distribute from project leader (R4Z0R49)
+*/
+
+//// TOGGLE DEBUGS ////
+
+// comment this out if you don't want any L related debug
+#define LOGIN_DEBUG
+
+// comment this out if you don't want any VEHICLE related debug
+#define VEHICLE_DEBUG
+
+// comment this out if you don't want any INVENTORY related debug
+#define VEHICLE_DEBUG
